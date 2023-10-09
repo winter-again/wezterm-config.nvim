3465e68084071d8ca14f99fc757a0b006d01907b
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2023-10-09 18:17:11 -0400

    Everything seems to be working properly at this point. Need to think more about the structure of the external profile data to ensure we keep things generic enough.

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,4 +1,3 @@
-local wezterm = require('wezterm')
 local M = {}
 
 M.set_background = function(config, backgrounds, bg)
@@ -7,25 +6,25 @@ M.set_background = function(config, backgrounds, bg)
     return config
 end
 
--- we're just trusting that user will specify proper
--- type of setting for a given config option
+-- we're just trusting that user will specify acceptable
+-- values for the config variable
+-- if not, then they have to clear the overrides before continuing
+-- to use the plugin
 local function simple_override(overrides, name, value)
     if value == 'true' or value == 'false' then
         value = value == 'true' -- convert to bool
-        print('value is bool')
+        -- print('value is bool')
     elseif string.match(value, '^%d*%.?%d+$') then
         value = tonumber(value) -- convert to numeric
-        print('value is numeric')
-    else
-        print('value is string')
+        -- print('value is numeric')
+    -- else
+        -- print('value is string')
     end
     overrides[name] = value
 
     return overrides
 end
 
--- TODO: figure out if there's a way to clear overrides and return to
--- defaults without having to restart wezterm; like passing an empty overrides table?
 M.user_var_override = function(overrides, name, value, profile_data)
     -- to draw a distinction between simple overrides and profile-type
     -- overrides, establish a required naming convention for user variables
@@ -42,7 +41,7 @@ M.user_var_override = function(overrides, name, value, profile_data)
         overrides = simple_override(overrides, name, value)
     end
 
-    print('override fired')
+    -- print('override fired')
 
     return overrides
 end
