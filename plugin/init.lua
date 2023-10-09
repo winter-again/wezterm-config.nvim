1635323df384cebd6aedabfb5dcb8db00a018e1e
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2023-10-09 16:57:31 -0400

    Core func working. Troubleshooting issue where user var values of wrong type are still having an effect

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,23 +1,30 @@
+local wezterm = require('wezterm')
 local M = {}
 
 M.set_background = function(config, backgrounds, bg)
     config.background = { backgrounds[bg] }
+
+    return config
 end
 
 -- we're just trusting that user will specify proper
 -- type of setting for a given config option
--- I'm guessing Wezterm will complain if a bad option is passed
 local function simple_override(overrides, name, value)
     if value == 'true' or value == 'false' then
         value = value == 'true' -- convert to bool
+        wezterm.log_info('bool')
     elseif string.match(value, '^%d*%.?%d+$') then
         value = tonumber(value) -- convert to numeric
+        wezterm.log_info('numeric')
     end
     overrides[name] = value
+    wezterm.log_info('string')
 
     return overrides
 end
 
+-- TODO: figure out if there's a way to clear overrides and return to
+-- defaults without having to restart wezterm; like passing an empty overrides table?
 M.user_var_override = function(overrides, name, value, profile_data)
     -- to draw a distinction between simple overrides and profile-type
     -- overrides, establish a required naming convention for user variables
@@ -28,7 +35,7 @@ M.user_var_override = function(overrides, name, value, profile_data)
         -- and then use the resulting option
         name = string.gsub(name, 'profile_', '') -- remove the prefix
         if name == 'background' then
-            M.set_background(overrides, profile_data.backgrounds, value)
+            overrides = M.set_background(overrides, profile_data.backgrounds, value)
         end
     else
         overrides = simple_override(overrides, name, value)
