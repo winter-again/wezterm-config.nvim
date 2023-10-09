6a902ff9c3b736366f44227ff49ed1cd8abb2aa8
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2023-10-09 14:20:07 -0400

    Try a slightly more generalizable way of specifying override

--- plugin/init.lua
+++ plugin/init.lua
@@ -6,11 +6,34 @@ M.set_background = function(config, choices, bg)
     }
 end
 
+-- we're just trusting that user will specify proper
+-- type of setting for a given config option
+-- I'm guessing Wezterm will complain if a bad option is passed
+local function simple_override(overrides, name, value)
+    if value == 'true' or value == 'false' then
+        value = value == 'true' -- convert to bool
+    elseif string.match(value, '^%d*%.?%d+$') then
+        value = tonumber(value) -- convert to numeric
+    end
+    overrides[name] = value
+
+    return overrides
+end
+
 M.user_var_override = function(overrides, name, value, choices)
-    if name == 'BG_CONFIG' then
-        M.set_background(overrides, choices, value)
-    elseif name == 'font_size' then
-        overrides.font_size = tonumber(value)
+    -- to draw a distinction between simple overrides and profile-type
+    -- overrides, establish a required naming convention for user variables
+    -- and use this to determine whether choices table needs to be utilized?
+    if string.match(name, '^profile_') then
+        -- instead of having to match each possible value
+        -- it makes more sense remove the 'profile_' prefix
+        -- and then use the resulting option
+        name = string.gsub(name, 'profile_', '') -- remove the prefix
+        if name == 'background' then
+            M.set_background(overrides, choices, value)
+        end
+    else
+        overrides = simple_override(overrides, name, value)
     end
 
     return overrides
