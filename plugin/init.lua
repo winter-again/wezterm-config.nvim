b367c8f5edbb17f2c949a062a829af3d2f55215f
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2023-10-09 20:25:53 -0400

    Trying out a more flexible framework for sending overrides

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,16 +1,10 @@
 local M = {}
 
-M.set_background = function(config, backgrounds, bg)
-    config.background = backgrounds[bg]
-
-    return config
-end
-
 -- we're just trusting that user will specify acceptable
 -- values for the config variable
 -- if not, then they have to clear the overrides before continuing
 -- to use the plugin
-local function simple_override(overrides, name, value)
+M.override_key_val = function(overrides, name, value)
     if value == 'true' or value == 'false' then
         value = value == 'true' -- convert to bool
         -- print('value is bool')
@@ -25,23 +19,33 @@ local function simple_override(overrides, name, value)
     return overrides
 end
 
-M.user_var_override = function(overrides, name, value, profile_data)
+-- support a handful of more complex overrides
+-- all of these require passing some "profile" data from within
+-- the wezterm config, which then allows users to pick one of
+-- their profiles as the override
+
+M.override_background = function(overrides, backgrounds, bg)
+    overrides.background = backgrounds[bg]
+
+    return overrides
+end
+
+M.override_user_var = function(overrides, name, value, profile_data)
     -- to draw a distinction between simple overrides and profile-type
     -- overrides, establish a required naming convention for user variables
-    -- and use this to determine whether choices table needs to be utilized?
+    -- if the user wants to use this function as a single entry point into
+    -- into the plugin
     if string.match(name, '^profile_') then
-        -- instead of having to match each possible value
-        -- it makes more sense remove the 'profile_' prefix
-        -- and then use the resulting option
-        name = string.gsub(name, 'profile_', '') -- remove the prefix
-        if name == 'background' then
-            overrides = M.set_background(overrides, profile_data.backgrounds, value)
+        local config_var = string.gsub(name, 'profile_', '') -- remove the prefix
+        -- there's probably a better way to know which variable-specific override
+        -- func to call instead of using series of if statements
+        -- need to handle for invalid config_var?
+        if config_var == 'background' then
+            overrides = M.override_background(overrides, profile_data.backgrounds, value)
         end
     else
-        overrides = simple_override(overrides, name, value)
+        overrides = M.override_key_val(overrides, name, value)
     end
-
-    -- print('override fired')
 
     return overrides
 end
