187e846dd16017c6c2ed931d66a154b92ab5f109
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2023-10-18 23:49:01 -0400

    Some clean up and testing if we can do any kind of checks on func input

--- lua/wezterm-config/init.lua
+++ lua/wezterm-config/init.lua
@@ -1,31 +1,34 @@
 local base64 = require('wezterm-config.base64_encode')
 local M = {}
 
--- TODO: add some logic to make sure value becomes or is a string?
+---sends a Wezterm config override from within Neovim
+---@param name string
+---@param value string
 function M.set_wezterm_user_var(name, value)
-    -- people have asked Wez about stuff like this before, to which he's linked
-    -- https://wezfurlong.org/wezterm/recipes/passing-data.html
-    -- his suggestions were implemented in this PR
-    -- https://github.com/folke/zen-mode.nvim/pull/61 which itself references a Reddit thread
-    -- https://www.reddit.com/r/neovim/comments/xn1q75/how_to_use_chansend/
-    -- Folke has kindly allowed me to adapt the code here
-    local stdout = vim.loop.new_tty(1, false)
-    local value_b64_enc = base64.encode(value)
-    if os.getenv('TMUX') then
-        -- unclear to me why using \033 isn't interpreted the same as \x1b
-        -- there are some files in nvim that seem like they could explain or have
-        -- something to do with nvim-specific interpretation, but I don't understand them
-        -- stdout:write(
-        --     ('\x1bPtmux;\x1b\x1b]1337;SetUserVar=%s=%s\007\x1b\\'):format(name, vim.fn.system({ 'base64' }, value))
-        -- )
+    -- TODO: should we instead auto convert all the args to strings?
+    if type(name) == 'string' and type(value) == 'string' then
+        -- people have asked Wez about stuff like this before, to which he's linked
+        -- https://wezfurlong.org/wezterm/recipes/passing-data.html
+        -- his suggestions were implemented in this PR
+        -- https://github.com/folke/zen-mode.nvim/pull/61 which itself references a Reddit thread
+        -- https://www.reddit.com/r/neovim/comments/xn1q75/how_to_use_chansend/
+        -- Folke has kindly allowed me to adapt the code here
+        local stdout = vim.loop.new_tty(1, false)
+        local value_b64_enc = base64.encode(value)
+        if os.getenv('TMUX') then
+            -- unclear to me why using \033 isn't interpreted the same as \x1b
+            -- there are some files in nvim that seem like they could explain or have
+            -- something to do with nvim-specific interpretation, but I don't understand them
 
-        -- this uses a Lua-only dep instead of requiring the user to have base64 in their path
-        stdout:write(('\x1bPtmux;\x1b\x1b]1337;SetUserVar=%s=%s\007\x1b\\'):format(name, value_b64_enc))
+            -- this uses a Lua-only dep instead of requiring the user to have base64 in their path
+            stdout:write(('\x1bPtmux;\x1b\x1b]1337;SetUserVar=%s=%s\007\x1b\\'):format(name, value_b64_enc))
+        else
+            stdout:write(('\x1b]1337;SetUserVar=%s=%s\007'):format(name, value_b64_enc))
+        end
+        stdout:close()
     else
-        -- stdout:write(('\x1b]1337;SetUserVar=%s=%s\007'):format(name, vim.fn.system({ 'base64' }, value)))
-        stdout:write(('\x1b]1337;SetUserVar=%s=%s\007'):format(name, value_b64_enc))
+        error('set_wezterm_user_var() only takes strings as arguments')
     end
-    stdout:close()
 end
 
 return M
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+-- TODO: can this be more robust?
 -- we're just trusting that user will specify acceptable
 -- values for the config variable
 -- if not, then they have to clear the overrides before continuing
@@ -7,52 +8,53 @@ local M = {}
 local function override_key_val(overrides, name, value)
     if value == 'true' or value == 'false' then
         value = value == 'true' -- convert to bool
-        -- print('value is bool')
     elseif string.match(value, '^%d*%.?%d+$') then
         value = tonumber(value) -- convert to numeric
-        -- print('value is numeric')
-        -- else
-        -- print('value is string')
     end
     overrides[name] = value
 
     return overrides
 end
 
--- support a handful of more complex overrides
--- all of these require passing some "profile" data from within
--- the wezterm config, which then allows users to pick one of
--- their profiles as the override
-
 local function override_profile(overrides, var, profile_data, sel)
-    -- print('var is ' .. var) -- 'background'
-    -- print('sel is ' .. sel) -- 'bg_1'
-    -- print(profile_data == nil)
     overrides[var] = profile_data[var][sel]
 
     return overrides
 end
 
--- TODO: add functionality to account for user
--- not specifying profile_data
-M.override_user_var = function(overrides, name, value, profile_data)
+local function check_profile_opt(opt)
+    local sup_opts = { 'colors', 'background' }
+    local is_sup = false
+    for _, sup_opt in ipairs(sup_opts) do
+        if opt == sup_opt then
+            is_sup = true
+        end
+    end
+
+    return is_sup
+end
+
+---interprets the Wezterm user var that got overridden and uses a specific helper
+---function to apply overrides to the passed overrides table, for use within
+---a callback function in Wezterm config
+---@param overrides table
+---@param name string
+---@param value string
+---@param profile_data table
+---@return table
+function M.override_user_var(overrides, name, value, profile_data)
+    -- TODO: add functionality to account for user
+    -- not specifying profile_data table
+
     -- to draw a distinction between simple overrides and profile-type
-    -- overrides, establish a required naming convention for user variables
-    -- if the user wants to use this function as a single entry point into
-    -- into the plugin
+    -- overrides, establish a required naming convention for certain user variables
+
     if string.match(name, '^profile_') then
         local config_var = string.gsub(name, 'profile_', '') -- remove the prefix
-        -- there's probably a better way to know which variable-specific override
-        -- func to call instead of using series of if statements
-        -- need to handle for invalid config_var?
-        -- there's also the expectation that profile_data has the fields ref below
-        -- if config_var == 'background' then
-        --     overrides = override_background(overrides, profile_data.backgrounds, value)
-        -- elseif config_var == 'colors' then
-        --     overrides = override_colors(overrides, profile_data.colors, value)
-        -- end
+        print('hello from 1')
         overrides = override_profile(overrides, config_var, profile_data, value)
     else
+        print('hello from 2')
         overrides = override_key_val(overrides, name, value)
     end
 
