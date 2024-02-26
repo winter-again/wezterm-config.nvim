0ef20c96198ec974de1fc9a537b7341d2a93d01c
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2024-02-26 18:21:06 -0500

    Check that wezterm plugin manager properly gets this version

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,19 +1,24 @@
-local M = {}
+-- TODO:
+-- simple workaround to avoid nvim trying to load this module
+-- https://github.com/wez/wezterm/issues/4533#issuecomment-1874094722
+-- is there a better alt?
+if vim ~= nil then
+    return
+end
 
--- TODO: can this be more robust?
--- we're just trusting that user will specify acceptable
--- values for the config variable
--- if not, then they have to clear the overrides before continuing
--- to use the plugin?
+local wezterm = require('wezterm')
+local M = {}
 
 ---@param overrides table
 ---@param name string
 ---@param value string
 local function override_key_val(overrides, name, value)
     if value == 'true' or value == 'false' then
-        value = value == 'true' -- convert to bool
+        -- convert to bool
+        value = value == 'true'
     elseif string.match(value, '^%d*%.?%d+$') then
-        value = tonumber(value) -- convert to numeric
+        -- convert to number
+        value = tonumber(value)
     end
     overrides[name] = value
     return overrides
@@ -59,41 +64,34 @@ function M.__override_user_var(overrides, name, value, profile_data)
     return overrides
 end
 
----@param name string
-local function is_sup_profile(name)
-    local sup_opts = { 'colors', 'background', 'font' }
-    for _, opt in ipairs(sup_opts) do
-        if opt == name then
-            return true
-        end
-    end
-    return false
-end
-
--- TODO: do we know for sure that user var is a string being passed in here?
---
 ---Interpret the Wezterm user var that got overridden and use a specific helper
 ---function to apply overrides to the passed overrides table; for use within
 ---a callback function in Wezterm config
 ---@param overrides table
 ---@param name string
 ---@param value string
----@param profile_data? table
 ---@return table
-function M.override_user_var(overrides, name, value, profile_data)
-    -- TODO: find cleaner way to have just one override_... func
-    -- and therefore optionally profile_data is optional
-    if profile_data ~= nil then
-        if is_sup_profile(name) then
-            overrides[name] = profile_data[name][value]
-            return overrides
-        else
-            -- this gets printed to Wezterm logs at the INFO level
-            -- because print is an alias for wezterm.log_info()
-            print("This profile option isn't currently supported")
-        end
+function M.override_user_var(overrides, name, value)
+    -- TODO: figure out how to just have one override func
+    -- aka detect whether the value passed is table or something simple
+    -- may have to put some condition on whether json_parse() works on the value?
+    -- or do a check before calling parse func
+
+    -- returns tbl if successfully parsed
+    -- otherwise it returns 1 (?) so I guess an error code or at least
+    -- something with type == 'number'
+    local parsed_val = wezterm.json_parse(value)
+    if type(parsed_val) == 'table' then
+        overrides[name] = parsed_val
     else
-        overrides = override_key_val(overrides, name, value)
+        if value == 'true' or value == 'false' then
+            -- convert to bool
+            value = value == 'true'
+        elseif string.match(value, '^%d*%.?%d+$') then
+            -- convert to number
+            value = tonumber(value)
+        end
+        overrides[name] = value
     end
     return overrides
 end
