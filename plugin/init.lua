d387ef50f3d135984530846a2b486f5e312880c9
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2024-02-26 02:16:03 -0500

    Trying to streamline the logic here

--- plugin/init.lua
+++ plugin/init.lua
@@ -5,6 +5,7 @@ local M = {}
 -- values for the config variable
 -- if not, then they have to clear the overrides before continuing
 -- to use the plugin
+-- maybe better to not do this manual type coercion
 local function override_key_val(overrides, name, value)
     if value == 'true' or value == 'false' then
         value = value == 'true' -- convert to bool
@@ -12,13 +13,11 @@ local function override_key_val(overrides, name, value)
         value = tonumber(value) -- convert to numeric
     end
     overrides[name] = value
-
     return overrides
 end
 
 local function override_profile(overrides, var, profile_data, sel)
     overrides[var] = profile_data[var][sel]
-
     return overrides
 end
 
@@ -30,7 +29,6 @@ local function check_profile_opt(opt)
             is_sup = true
         end
     end
-
     return is_sup
 end
 
@@ -42,10 +40,7 @@ end
 ---@param value string
 ---@param profile_data table
 ---@return table
-function M.override_user_var(overrides, name, value, profile_data)
-    -- TODO: maybe the simpler way instead of forcing this 'profile_...' stuff
-    -- is to manually check the raw var name to see if it's one of the ones
-    -- that accepts a table and then do diff logic based on that check
+function M.__override_user_var(overrides, name, value, profile_data)
     if string.match(name, '^profile_') then
         local config_var = string.gsub(name, 'profile_', '') -- remove the prefix
         if check_profile_opt(config_var) then
@@ -58,7 +53,43 @@ function M.override_user_var(overrides, name, value, profile_data)
     else
         overrides = override_key_val(overrides, name, value)
     end
+    return overrides
+end
 
+---@param name string
+local function is_sup_profile(name)
+    local sup_opts = { 'colors', 'background', 'font' }
+    for _, opt in ipairs(sup_opts) do
+        if opt == name then
+            return true
+        end
+    end
+    return false
+end
+
+---interprets the Wezterm user var that got overridden and uses a specific helper
+---function to apply overrides to the passed overrides table, for use within
+---a callback function in Wezterm config
+---@param overrides table
+---@param name string
+---@param value string
+---@param profile_data? table
+---@return table
+function M.override_user_var(overrides, name, value, profile_data)
+    -- TODO: find cleaner way to have just one override_... func
+    -- and therefore optionally profile_data is optional
+    if profile_data ~= nil then
+        if is_sup_profile(name) then
+            overrides[name] = profile_data[name][value]
+            return overrides
+        else
+            -- this gets printed to Wezterm logs at the INFO level
+            -- because print is an alias for wezterm.log_info()
+            print("This profile option isn't currently supported")
+        end
+    else
+        overrides = override_key_val(overrides, name, value)
+    end
     return overrides
 end
 
